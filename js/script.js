1cf2b667593e35a27618c831175c2c2c8a3288e1
Huan Azevedo <huancarlos89@gmail.com>
2021-09-14 12:11:40 -0300

    Criação do movimento da cobrinha

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
 function criarBG(){
     context.fillStyle = "lightgreen"; // Cor do Context
@@ -19,5 +20,26 @@ function criarCobrinha(){
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x; // Posição inicial X
+    let snakeY = snake[0].y; // Posição inicial Y
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop(); // Retira o ultimo elemento do array snake[]
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100); // Iniciar o jogo a cada milissegundos
